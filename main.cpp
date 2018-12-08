62b2359a412f44c3aebd6e6045e7a5c5f7f29e55
ArthurGoodman <arthur.goodman3@gmail.com>
2018-12-08 23:58:12 +0200

    Everything else

--- main.cpp
+++ main.cpp
@@ -3,89 +3,204 @@
 #include <QtCore/QDebug>
 #include <QtCore/QElapsedTimer>
 #include <QtCore/QTimer>
+#include <QtGui/QFontDatabase>
 #include <QtGui/QKeyEvent>
 #include <QtGui/QPainter>
 #include <QtGui/QPainterPath>
 #include <QtWidgets/QApplication>
 #include <QtWidgets/QWidget>
-#include "Types.hpp"
 
 namespace {
+
+static constexpr double c_epsilon = 1e-4;
+static constexpr double c_wheel_base = 40;
+static constexpr double c_max_speed = 5000;
+static constexpr double c_max_acceleration = 1e3;
+static constexpr double c_max_wheel_angle = M_PI / 5;
+
+struct VehicleState
+{
+    Eigen::Vector2d position;
+    double speed;
+    double acceleration;
+    double heading;
+    double wheel_angle;
+};
 
 class CVehicleController final
 {
 public: // methods
     explicit CVehicleController()
+        : m_state{}
     {
+        m_state.heading = -M_PI / 2;
     }
 
     void update(double dt)
     {
-        Eigen::Vector2d a{0.0, 0.0};
+        auto x = m_state.position;
+        auto v = m_state.speed;
+        auto a = m_state.acceleration;
+        auto alpha = m_state.heading;
+        auto theta = m_state.wheel_angle;
 
-        if (m_up_button_pressed)
+        if (m_breaking)
         {
-            a -= Eigen::Vector2d{0, 1} * 1e4;
+            static constexpr double c_breaking_time = 1.0;
+            static constexpr double c_acceleration =
+                c_max_speed / c_breaking_time;
+
+            if (std::abs(v) > c_epsilon)
+            {
+                double sign = v > 0 ? -1 : 1;
+                v += sign * c_acceleration * dt;
+            }
+            else
+            {
+                v = 0.0;
+            }
+        }
+        else
+        {
+            static constexpr double c_acceleration_time = 0.25;
+            static constexpr double c_jerk =
+                c_max_acceleration / c_acceleration_time;
+
+            if (m_accelerating || m_accelerating_backwards)
+            {
+                if (m_accelerating)
+                {
+                    a += c_jerk * dt;
+                }
+                else
+                {
+                    a -= c_jerk * dt;
+                }
+            }
+            else
+            {
+                if (std::abs(a) > c_epsilon)
+                {
+                    double sign = a > 0 ? -1 : 1;
+                    a += sign * c_jerk * dt;
+                }
+                else
+                {
+                    a = 0.0;
+                }
+            }
         }
 
-        if (m_down_button_pressed)
+        if (m_turning_left)
         {
-            a += Eigen::Vector2d{0, 1} * 1e4;
+            static constexpr double c_steering_time = 0.5;
+            const double c_steering_rate = c_max_wheel_angle / c_steering_time *
+                                           (1.1 - std::abs(v) / c_max_speed);
+
+            theta -= c_steering_rate * dt;
+        }
+        else if (m_turning_right)
+        {
+            static constexpr double c_steering_time = 0.5;
+            const double c_steering_rate = c_max_wheel_angle / c_steering_time *
+                                           (1.1 - std::abs(v) / c_max_speed);
+
+            theta += c_steering_rate * dt;
+        }
+        else if (std::abs(theta) > c_epsilon)
+        {
+            static constexpr double c_steering_time = 0.125;
+            const double c_steering_rate =
+                c_max_wheel_angle / c_steering_time * std::abs(v) / c_max_speed;
+
+            const double sign = theta > 0 ? -1 : 1;
+            theta += sign * c_steering_rate * dt;
+        }
+        else
+        {
+            theta = 0.0;
         }
 
-        double w = 0.0;
+        const double w = std::tan(theta) / c_wheel_base * v;
 
-        if (m_left_button_pressed)
+        if (std::abs(w) <= c_epsilon)
         {
-            w -= 1e3;
+            const Eigen::Rotation2Dd R{alpha};
+            m_state.position =
+                x + R * Eigen::Vector2d{v * dt + a * dt * dt / 2, 0.0};
+        }
+        else
+        {
+            const double delta_x =
+                1.0 / w / w *
+                ((v * w + a * w * dt) * std::sin(alpha + w * dt) +
+                 a * std::cos(alpha + w * dt) - v * w * std::sin(alpha) -
+                 a * std::cos(alpha));
+            const double delta_y =
+                1.0 / w / w *
+                ((-v * w - a * w * dt) * std::cos(alpha + w * dt) +
+                 a * std::sin(alpha + w * dt) + v * w * std::cos(alpha) -
+                 a * std::sin(alpha));
+
+            m_state.position = x + Eigen::Vector2d{delta_x, delta_y};
         }
 
-        if (m_right_button_pressed)
-        {
-            w += 1e3;
-        }
+        m_state.speed = v + a * dt;
 
-        auto position = m_state.getPosition();
-        auto velocity = m_state.getVelocity();
-        double rotation = m_state.getRotation().x();
-        const Eigen::Rotation2Dd R{rotation * M_PI / 180};
+        m_state.speed =
+            std::max(-c_max_speed, std::min(m_state.speed, c_max_speed));
 
-        m_state.setPosition(position + R * velocity * dt);
-        m_state.setVelocity((velocity + a * dt) * (1 - 1 * dt));
-        m_state.setRotation(Vector1d{rotation + w * dt});
+        m_state.acceleration = a;
+        m_state.acceleration = std::max(
+            -c_max_acceleration,
+            std::min(m_state.acceleration, c_max_acceleration));
+
+        m_state.heading = alpha + w * dt;
+        m_state.heading = std::fmod(m_state.heading + 2 * M_PI, 2 * M_PI);
+
+        m_state.wheel_angle = theta;
+        m_state.wheel_angle = std::max(
+            -c_max_wheel_angle,
+            std::min(m_state.wheel_angle, c_max_wheel_angle));
     }
 
-    CVehicleState getState() const
+    VehicleState getState() const
     {
         return m_state;
     }
 
-    void setLeftButtonState(bool pressed)
+    void setTurningLeft(bool value)
     {
-        m_left_button_pressed = pressed;
+        m_turning_left = value;
     }
 
-    void setRightButtonState(bool pressed)
+    void setTurningRight(bool value)
     {
-        m_right_button_pressed = pressed;
+        m_turning_right = value;
     }
 
-    void setUpButtonState(bool pressed)
+    void setAccelerating(bool value)
     {
-        m_up_button_pressed = pressed;
+        m_accelerating = value;
     }
 
-    void setDownButtonState(bool pressed)
+    void setAcceleratingBackwards(bool value)
     {
-        m_down_button_pressed = pressed;
+        m_accelerating_backwards = value;
+    }
+
+    void setBreaking(bool value)
+    {
+        m_breaking = value;
     }
 
 private: // fields
-    CVehicleState m_state;
-    bool m_left_button_pressed = false;
-    bool m_right_button_pressed = false;
-    bool m_up_button_pressed = false;
-    bool m_down_button_pressed = false;
+    VehicleState m_state;
+    bool m_turning_left = false;
+    bool m_turning_right = false;
+    bool m_accelerating = false;
+    bool m_accelerating_backwards = false;
+    bool m_breaking = false;
 };
 
 class CWidget final : public QWidget
@@ -99,7 +214,8 @@ public: // methods
 protected: // methods
     void timerEvent(QTimerEvent *) override
     {
-        double dt = m_timer.nsecsElapsed() * 1e-9;
+        static constexpr double c_nsecs_in_sec = 1e-9;
+        double dt = m_timer.nsecsElapsed() * c_nsecs_in_sec;
         m_controller.update(dt);
         m_timer.restart();
     }
@@ -141,19 +257,23 @@ protected: // methods
             switch (e->key())
             {
             case Qt::Key_Left:
-                m_controller.setLeftButtonState(true);
+                m_controller.setTurningLeft(true);
                 break;
 
             case Qt::Key_Right:
-                m_controller.setRightButtonState(true);
+                m_controller.setTurningRight(true);
                 break;
 
             case Qt::Key_Up:
-                m_controller.setUpButtonState(true);
+                m_controller.setAccelerating(true);
                 break;
 
             case Qt::Key_Down:
-                m_controller.setDownButtonState(true);
+                m_controller.setAcceleratingBackwards(true);
+                break;
+
+            case Qt::Key_Space:
+                m_controller.setBreaking(true);
                 break;
             }
         }
@@ -166,19 +286,23 @@ protected: // methods
             switch (e->key())
             {
             case Qt::Key_Left:
-                m_controller.setLeftButtonState(false);
+                m_controller.setTurningLeft(false);
                 break;
 
             case Qt::Key_Right:
-                m_controller.setRightButtonState(false);
+                m_controller.setTurningRight(false);
                 break;
 
             case Qt::Key_Up:
-                m_controller.setUpButtonState(false);
+                m_controller.setAccelerating(false);
                 break;
 
             case Qt::Key_Down:
-                m_controller.setDownButtonState(false);
+                m_controller.setAcceleratingBackwards(false);
+                break;
+
+            case Qt::Key_Space:
+                m_controller.setBreaking(false);
                 break;
             }
         }
@@ -186,21 +310,37 @@ protected: // methods
 
     void paintEvent(QPaintEvent *) override
     {
-        static constexpr double c_scale = 2.0;
+        static constexpr double c_car_width = c_wheel_base / 1.75;
 
-        CVehicleState vehicle_state = m_controller.getState();
+        VehicleState vehicle_state = m_controller.getState();
 
-        QPointF pos{vehicle_state.getPosition().x(),
-                    vehicle_state.getPosition().y()};
+        auto x = vehicle_state.position;
+        double alpha = vehicle_state.heading;
+        const Eigen::Rotation2Dd R{alpha};
+        x += R * Eigen::Vector2d{c_wheel_base / 2, 0.0};
+
+        QPointF pos{x.x(), x.y()};
 
         static constexpr QRgb c_bg_color = qRgb(30, 30, 30);
 
         QPainter p{this};
         p.fillRect(rect(), c_bg_color);
 
-        p.translate(rect().center() - pos);
+        p.translate(rect().center());
+
+        static QPointF camera_pos = -pos;
+        double c_cam_alpha = std::pow(
+            std::atan(QVector2D(-pos - camera_pos).length() / height()) / M_PI *
+                2,
+            1.5);
+        camera_pos = c_cam_alpha * -pos + (1.0 - c_cam_alpha) * camera_pos;
 
         m_trajectory.emplace_back(pos);
+
+        p.setPen(QPen{Qt::white, 2});
+
+        p.save();
+        p.translate(camera_pos);
 
         QPainterPath path;
         if (!m_trajectory.empty())
@@ -213,12 +353,70 @@ protected: // methods
         }
         p.strokePath(path, QPen{Qt::gray, 1});
 
-        p.translate(pos);
-        p.rotate(vehicle_state.getRotation().x());
-        p.scale(c_scale, c_scale);
+        QPointF base_pos{vehicle_state.position.x(),
+                         vehicle_state.position.y()};
+        p.translate(base_pos);
+        p.rotate(vehicle_state.heading / M_PI * 180);
+        p.drawRect(QRect(0, -c_car_width / 2, c_wheel_base, c_car_width));
+        p.save();
+        p.translate(c_wheel_base, -c_car_width / 2);
+        p.rotate(vehicle_state.wheel_angle / M_PI * 180);
+        p.fillRect(
+            QRect(
+                -c_wheel_base / 8,
+                -c_car_width / 8,
+                c_wheel_base / 4,
+                c_car_width / 4),
+            Qt::white);
+        p.restore();
+        p.save();
+        p.translate(c_wheel_base, c_car_width / 2);
+        p.rotate(vehicle_state.wheel_angle / M_PI * 180);
+        p.fillRect(
+            QRect(
+                -c_wheel_base / 8,
+                -c_car_width / 8,
+                c_wheel_base / 4,
+                c_car_width / 4),
+            Qt::white);
+        p.restore();
+        p.save();
+        p.translate(0, -c_car_width / 2);
+        p.fillRect(
+            QRect(
+                -c_wheel_base / 8,
+                -c_car_width / 8,
+                c_wheel_base / 4,
+                c_car_width / 4),
+            Qt::white);
+        p.restore();
+        p.save();
+        p.translate(0, c_car_width / 2);
+        p.fillRect(
+            QRect(
+                -c_wheel_base / 8,
+                -c_car_width / 8,
+                c_wheel_base / 4,
+                c_car_width / 4),
+            Qt::white);
+        p.restore();
+        p.restore();
 
-        p.setPen(Qt::white);
-        p.drawRect(QRect(-5, -10, 10, 20));
+        QPointF text_pos = QPointF{200, -200};
+
+        QString info;
+        info.sprintf(
+            "Speed: %.04f pix/s\n"
+            "Acceleration: %.04f pix/s^2\n"
+            "Heading: %.04f deg\n"
+            "Wheel angle: %.04f deg\n",
+            vehicle_state.speed,
+            vehicle_state.acceleration,
+            vehicle_state.heading / M_PI * 180,
+            vehicle_state.wheel_angle / M_PI * 180);
+
+        p.setFont(QFontDatabase::systemFont(QFontDatabase::FixedFont));
+        p.drawText(QRect(text_pos.x(), text_pos.y(), 1000, 1000), info);
 
         QTimer::singleShot(16, this, SLOT(update()));
     }
